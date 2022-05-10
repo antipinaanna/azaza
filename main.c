e5a187b0b9f8a1721bb3621f41469a5985ece79d
antipinaanna <102436052+antipinaanna@users.noreply.github.com>
2022-05-10 22:07:16 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #define STB_IMAGE_IMPLEMENTATION
+#include <math.h>
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-#include math.h
+
 #include<strings.h>
 #include<stdio.h>
 #include<stdlib.h>
