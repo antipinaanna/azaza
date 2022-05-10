6ed3319ebf562ed1f2254e663b3f1f7b7c0edfd9
antipinaanna <102436052+antipinaanna@users.noreply.github.com>
2022-05-10 22:58:44 +0300

    Update main.c

--- main.c
+++ main.c
@@ -24,8 +24,8 @@ int main(void) {
   
   int k = 0;
   for (int i = 0; i < img_size; i += 3) {
-    //gray_img[k++] = (img[i] * 11 + img[i + 1] * 16 + img[i + 2] * 5) / 32;
-    gray_img[k++] = 255;
+    gray_img[k++] = (img[i] * 11 + img[i + 1] * 16 + img[i + 2] * 5) / 32;
+    //gray_img[k++] = 255;
   }
   
   stbi_write_jpg("hamster_gray.jpg", width, height, 1, gray_img, 100);
