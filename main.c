874ce8c12a564d1b4b284875276fac9e9b4f9e91
antipinaanna <102436052+antipinaanna@users.noreply.github.com>
2022-05-10 22:32:38 +0300

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,19 @@
 #include<stdlib.h>
 
 int main(){
-  printf("hello world!\n");
+  
   return 0;
   
+}
+
+int main(void) {
+  printf("hello world!\n");
+  int width, height, channels;
+  unsigned char *img = stbi_load("sky.jpg", &width, &height, &channels, 0);
+  if(img == NULL) {
+      printf("Error in loading the image\n");
+      exit(1);
+  }
+  printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
+ 
 }
