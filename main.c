b3331af75a11d56b06c1cea7e386233ad4443c10
antipinaanna <102436052+antipinaanna@users.noreply.github.com>
2022-05-10 22:47:50 +0300

    Update main.c

--- main.c
+++ main.c
@@ -17,6 +17,17 @@ int main(void) {
       printf("Error in loading the image\n");
       exit(1);
   }
+  
+  size_t gray_img_size = width * height;
+  unsigned char *gray_img = malloc(gray_img_size);
+  
+  int k = 0;
+  for (int i = 0; i < gray_img_size; i += 3) {
+    gray_img[k++] = (img[i] * 11 + img[i + 1] * 16 + img[i + 2] * 5) / 32;
+  }
+  
+  stbi_write_jpg("hamster_gray.jpg", width, height, 1, gray_img, 100);
+  
   printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
  
 }
