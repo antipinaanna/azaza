1df5c9501a9a9de502d3109304dd022a1dd47dcc
antipinaanna <102436052+antipinaanna@users.noreply.github.com>
2022-06-10 21:41:19 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #define STB_IMAGE_IMPLEMENTATION
 #include <math.h>
 #include "stb_image.h"
@@ -9,29 +10,24 @@
 #include<stdlib.h>
 
 
-
 int main(void) {
   printf("hello world!\n");
-  size_t width, height, channels;
+  int width, height, channels;
   unsigned char *img = stbi_load("hamster.jpg", &width, &height, &channels, 0);
   if(img == NULL) {
       printf("Error in loading the image\n");
       exit(1);
   }
+  
   size_t img_size = width * height * channels;
   size_t gray_img_size = width * height;
-  unsigned char *gray_img = (unsigned char *)malloc(gray_img_size*sizeof(unsigned char));
-  printf("hello world!\n");
-  size_t k = 0, i, j;  
-  printf ("%ld %ld\n", width, height); 
-  printf ("%ld %ld\n", gray_img_size, img_size);
-  for (i = 0; i < img_size - 3; i += 3) {
-//    printf ("%d %d\n", i, k);
+  unsigned char *gray_img = malloc(gray_img_size);
+  
+  int k = 0;
+  for (int i = 0; i < img_size; i += 3) {
     gray_img[k++] = (img[i] * 11 + img[i + 1] * 16 + img[i + 2] * 5) / 32;
-//    gray_img[k++] = 255;
+    //gray_img[k++] = 255;
   }
-    printf ("1");
-
   
   stbi_write_jpg("hamster_gray.jpg", width, height, 1, gray_img, 100);
   
