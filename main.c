065f7e60acb757d6edf09df4937a83a4a0d057f6
antipinaanna <102436052+antipinaanna@users.noreply.github.com>
2022-06-28 20:53:52 +0300

    Update main.c

--- main.c
+++ main.c
@@ -89,7 +89,7 @@ int main(void) {
         graph[i][j] = gray_img[i * width + j];
       }
     printf ("3\n");
-   high_contrast (unsigned char** graph, int w, int h, int black, int white);
+   high_contrast (graph, w, h, black, white);
    Gauss_blur(width, height, graph, graph1);
    printf ("7\n");
     for (i = 0; i < height; i++)
