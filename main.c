06b7fb202ce1895975e6390678bccdbd326880c4
antipinaanna <102436052+antipinaanna@users.noreply.github.com>
2022-07-05 18:21:07 +0300

    Update main.c

--- main.c
+++ main.c
@@ -74,22 +74,22 @@ void dfs2 (int i, int j, int w, int h, unsigned char** data, int** v, int m, int
     
     v[i][j] = m;
     if((i - 1 > 0) && (i - 1 < h) && (j - 1 > 0) && (j - 1 < w)){
-        if((abs(data[i][j]-data[i - 1][j - 1]) <= 20) && (v[i - 1][j - 1] == 0)){
+        if((abs(data[i][j]-data[i - 1][j - 1]) <= 2) && (v[i - 1][j - 1] == 0)){
             dfs2(i - 1, j - 1, w, h, data, v, m, f + 1);
         }
     }
     if((i + 1 > 0) && (i + 1 < h) && (j > 0) && (j < w)){
-        if((abs(data[i][j]-data[i + 1][j]) <= 20) && (v[i + 1][j] == 0)){
+        if((abs(data[i][j]-data[i + 1][j]) <= 2) && (v[i + 1][j] == 0)){
             dfs2(i + 1, j, w, h, data, v, m, f + 1);
         }
     }
     if((i - 1 > 0) && (i - 1 < h) && (j + 1 > 0) && (j + 1 < w)){
-        if((abs(data[i][j]-data[i - 1][j + 1]) <= 20) && (v[i - 1][j + 1] == 0)){
+        if((abs(data[i][j]-data[i - 1][j + 1]) <= 2) && (v[i - 1][j + 1] == 0)){
             dfs2(i - 1, j + 1, w, h, data, v, m, f + 1);
         }
     }
     if((i > 0) && (i < h) && (j + 1> 0) && (j + 1 < w)){
-        if((abs(data[i][j]-data[i][j + 1]) <= 20) && (v[i][j + 1] == 0)){
+        if((abs(data[i][j]-data[i][j + 1]) <= 2) && (v[i][j + 1] == 0)){
             dfs2(i, j + 1, w, h, data, v, m, f + 1);
         }
     }
